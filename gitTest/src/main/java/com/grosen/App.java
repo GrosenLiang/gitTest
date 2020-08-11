256c54cdf636bbe8e7f377a2dfe1fc53475c21cd
GrosenLiang <151865933@qq.com>
2020-08-11 15:42:29 +0800

    创建dev分支

--- gitTest/src/main/java/com/grosen/App.java
+++ gitTest/src/main/java/com/grosen/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("Hello Git!");
         System.out.println("更新1");
+        System.out.println("创建dev分支");
     }
 }
